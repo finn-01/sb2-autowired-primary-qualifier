aae29d2228d15a1e196e6af85373e397a2cd96b2
finn-01 <nguyensam576@gmail.com>
2022-02-11 20:53:37 +0700

    create project

---
+++ src/main/java/com/spring/sb2autowiredprimaryqualifier/Sb2AutowiredPrimaryQualifierApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sb2autowiredprimaryqualifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2AutowiredPrimaryQualifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb2AutowiredPrimaryQualifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/sb2autowiredprimaryqualifier/Sb2AutowiredPrimaryQualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sb2autowiredprimaryqualifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2AutowiredPrimaryQualifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
